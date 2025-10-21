531bf8eff0f89cd6714c2fcb63a06952ee4d5f3b
Patrick Owen <patrickowen861@gmail.com>
2025-10-21 17:33:03 +0800

    improved script function for close buttons

--- interactions.js
+++ interactions.js
@@ -8,20 +8,21 @@ function gotoWorks() {
     document.getElementById('my-works').style.display= 'flex';
 }
 
-function returnAbout() {
-    document.getElementById('about-me').style.display = 'none';
-    document.getElementById('home').style.display='flex';
-}
-
-function returnWorks() {
-    document.getElementById('my-works').style.display = 'none';
-    document.getElementById('home').style.display='flex';
-}
-
 function visitGithub() {
     window.open('https://github.com/TeLeViSiOn161511', '_blank')
 }
 
 function visitLinkedin() {
     window.open('https://www.linkedin.com/in/patrickowenkuek/', '_blank');
+}
+
+// returning containers to Home
+const closeButton = document.querySelectorAll(".close");
+
+for (let i = 0; i < closeButton.length; i++) {
+    closeButton[i].addEventListener('click', function() {
+        const sectionId = this.dataset.section;
+        document.querySelector("#" + sectionId).style.display = 'none';
+        document.querySelector("#home").style.display = 'flex';
+    })
 }
